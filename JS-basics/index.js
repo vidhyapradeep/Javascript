dfe806bb7cf5618d10b858f50f6009caa400fa95
vidhyapradeep <vidhyakiruthi2@gmail.com>
2021-05-29 19:11:22 +0800

    Enumerating prop of an object

--- JS-basics/index.js
+++ JS-basics/index.js
@@ -21,3 +21,19 @@ function Circle(radius){
     }
 }
 const myCircle = new Circle(1);
+
+//Enumerating properties of an object
+const circle1 = {
+    radius: 1,
+    draw(){
+        console.log('DRAW');
+    } 
+}
+for(let key in circle1)
+    console.log(key, circle1[key]);
+
+for(let key of Object.keys(circle1))
+    console.log(key);    
+
+for(let entry of Object.entries(circle1))
+    console.log('entry', entry)    
