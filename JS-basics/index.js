1cceee2543e3dd91071c26930546c331cdea662e
vidhyapradeep <vidhyakiruthi2@gmail.com>
2021-05-29 17:01:49 +0800

    Factory Functions and Constructor Functions

--- JS-basics/index.js
+++ JS-basics/index.js
@@ -1,2 +1,23 @@
 let name;
-console.log(name);
+//console.log(name);
+
+
+// Factory Function
+function createCircle(radius){
+    return {
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    }
+}
+const circle = createCircle(1);
+
+// Constructor Function
+function Circle(radius){
+    this.radius = radius,
+    this.draw = function(){
+        console.log('Draw - Constructor function');
+    }
+}
+const myCircle = new Circle(1);
